420b8d426eb1528a33bd4394f8a6f0e7447aab84
ParkYoungHoon <hoon2585@gmail.com>
2016-09-26 10:48:55 +0900

    Delete MainActivity, activity_main Create LoginActivity, activity_login Add LoginActivity in AndroidManifest

---
+++ LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.dudgns0507.learndoin.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dudgns0507.learndoin.R;
+
+/**
+ * Created by pyh42 on 2016-09-26.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
