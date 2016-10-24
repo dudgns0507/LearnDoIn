775242342dfb3b3c14984ac91c8362982a83e992
dudgns0507 <hoon2585@gmail.com>
2016-10-24 22:21:00 +0900

    Edit AndroidManifest, Add font files, Add Activity Java Files and xml Files
    Add font files - type otf
    Add WRITE_EXTERNAL_STORAGE Permission

--- LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/SettingActivity.java
+++ LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/SettingActivity.java
@@ -1,23 +1,19 @@
 package com.example.dudgns0507.learndoin.Activity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.dudgns0507.learndoin.R;
-import com.tsengvn.typekit.Typekit;
-import com.tsengvn.typekit.TypekitContextWrapper;
 
 /**
- * Created by pyh42 on 2016-09-26.
+ * Created by pyh42 on 2016-10-24.
  */
 
-public class LoginActivity extends AppCompatActivity {
-
+public class SettingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_setting);
     }
 }
