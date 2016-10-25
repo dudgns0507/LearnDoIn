5f9e9582a37fcffc8918c031886400b1db6074d7
dudgns0507 <hoon2585@gmail.com>
2016-10-25 18:20:39 +0900

    Edit LoginActivity, activity_login, FileManage

--- LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/FileManage.java
+++ LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/FileManage.java
@@ -8,10 +8,13 @@ import java.util.StringTokenizer;
  * Created by sonbi on 2016-10-24.
  */
 public class FileManage {
+
+    public static FileManage instace;
+
     public FileManage()
     {
-
     }
+
     public Map<String,String> parse(String data, String replace)
     {
         Map<String,String> wordData = new HashMap<String,String>();
--- LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/LoginActivity.java
+++ LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.dudgns0507.learndoin.Activity;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.dudgns0507.learndoin.R;
 import com.tsengvn.typekit.Typekit;
@@ -19,5 +23,33 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    void init() {
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.hide();
+
+        setFont("NanumBarunGothicUltraLight.otf", R.id.main_title);
+        setFont("NanumBarunGothicUltraLight.otf", R.id.sub_title);
+        setFont("NanumBarunGothicLight.otf", R.id.login_btn);
+        setFont("NanumBarunGothicBold.otf", R.id.google_login_btn);
+        setFont("NanumBarunGothicBold.otf", R.id.faceook_login_btn);
+        setFont("NanumBarunGothicLight.otf", R.id.registration_btn);
+        setFont("NanumBarunGothicLight.otf", R.id.finduser_btn);
+    }
+
+    void setFont(String path, int res) {
+        Typeface font = Typeface.createFromAsset(this.getAssets(), path);
+
+        if(findViewById(res) instanceof TextView) {
+            TextView mTextView = (TextView) findViewById(res);
+            mTextView.setTypeface(font);
+        }
+        if(findViewById(res) instanceof Button) {
+            Button mButton = (Button)findViewById(res);
+            mButton.setTypeface(font);
+        }
     }
 }
