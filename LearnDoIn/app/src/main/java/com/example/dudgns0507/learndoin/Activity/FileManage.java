a36abf7a95071eeb4f7e208ada9140f4368f13a6
DESKTOP-5VETL7L\sonbi <sonbill971@gmail.com>
2016-11-07 10:40:56 +0900

    Deformed FileManage Class for using WordClass

--- LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/FileManage.java
+++ LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/FileManage.java
@@ -1,6 +1,8 @@
 package com.example.dudgns0507.learndoin.Activity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
@@ -15,16 +17,17 @@ public class FileManage {
     {
     }
 
-    public Map<String,String> parse(String data, String replace)
+    public List<WordClass> parse(String data, String replace)
     {
-        Map<String,String> wordData = new HashMap<String,String>();
+        List<WordClass> wordList = new ArrayList<>(); // word, definition, time
         StringTokenizer tokenizer = new StringTokenizer(data,replace);
         for(int i=0;i<=tokenizer.countTokens();++i){
-            String key = tokenizer.nextToken();
-            String value = tokenizer.nextToken();
-            wordData.put(key,value);
+            WordClass temp = new WordClass();
+            temp.wordContent = tokenizer.nextToken();
+            temp.meaning = tokenizer.nextToken();
+            wordList.add(temp);
         }
-        return wordData;
+        return wordList;
     }
 }
 
