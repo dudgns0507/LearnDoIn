c6530df959924659f7ea406e58c70bccbab9866b
dudgns0507 <hoon2585@gmail.com>
2016-11-11 16:29:16 +0900

    ListActivity Background crash issue fixed

--- LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/ListActivity.java
+++ LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/Activity/ListActivity.java
@@ -1,9 +1,16 @@
 package com.example.dudgns0507.learndoin.Activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.dudgns0507.learndoin.R;
 
@@ -12,6 +19,9 @@ import com.example.dudgns0507.learndoin.R;
  */
 
 public class ListActivity extends AppCompatActivity {
+
+    private final static  String TAG = "ListActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +33,37 @@ public class ListActivity extends AppCompatActivity {
     void init() {
         ActionBar mActionBar = getSupportActionBar();
         mActionBar.hide();
+
+        putBitmap(R.id.list_top, R.drawable.back_1, 1);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.w(TAG, "Destroy background");
+        recycleView(R.id.list_top);
+        super.onDestroy();
+    }
+
+    private void recycleView(int id) {
+        ImageView view = (ImageView)findViewById(id);
+
+        Drawable d = view.getDrawable();
+        if(d instanceof BitmapDrawable) {
+            Bitmap b = ((BitmapDrawable) d).getBitmap();
+            view.setImageBitmap(null);
+            b.recycle();
+            b = null;
+        }
+        d.setCallback(null);
+        System.gc();
+        Runtime.getRuntime().gc();
+    }
+
+    private void putBitmap(int imageViewId, int drawableId, int scale) {
+        ImageView imageView = (ImageView)findViewById(imageViewId);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = scale;
+
+        imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), drawableId, options));
     }
 }
